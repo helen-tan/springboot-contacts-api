fb4af7ab05844bcf38b7b7f10ae5f486a1c535a3
helen-tan <helentanmz0909@gmail.com>
2022-12-20 10:57:02 +0800

    new spring boot project

---
+++ src/main/java/com/springbootcontactsapi/springbootcontactsapi/SpringbootContactsApiApplication.java
@@ -0,0 +1,13 @@
+package com.springbootcontactsapi.springbootcontactsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootContactsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootContactsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootcontactsapi/springbootcontactsapi/SpringbootContactsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootcontactsapi.springbootcontactsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootContactsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
