add4a017259118bb9d8ee07ed478a791102a4020
helen-tan <helentanmz0909@gmail.com>
2022-12-20 16:38:34 +0800

    Update a contact

--- src/main/java/com/springbootcontactsapi/springbootcontactsapi/controller/ContactController.java
+++ src/main/java/com/springbootcontactsapi/springbootcontactsapi/controller/ContactController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class ContactController {
     @Autowired
     private ContactService contactService;
 
+    // Get a Contact by id
     @GetMapping("/contact/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable String id) {
         Contact contact = contactService.getContactById(id);
@@ -25,10 +27,20 @@ public class ContactController {
         return new ResponseEntity<>(contact, HttpStatus.OK);
     }
 
+    // Create a contact
     @PostMapping("/contact")
     public ResponseEntity<HttpStatus> createContact(@RequestBody Contact contact) {
         contactService.saveContact(contact);
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    // Update a contact
+    @PutMapping("/contact/{id}")
+    public ResponseEntity<Contact> updateContact(@PathVariable String id, @RequestBody Contact contact) {
+        contactService.updateContact(id, contact);
+
+        return new ResponseEntity<>(contactService.getContactById(id), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/springbootcontactsapi/springbootcontactsapi/service/ContactService.java
+++ src/main/java/com/springbootcontactsapi/springbootcontactsapi/service/ContactService.java
@@ -6,4 +6,6 @@ public interface ContactService {
     Contact getContactById(String id);
 
     void saveContact(Contact contact);
+
+    void updateContact(String id, Contact contact);
 }
